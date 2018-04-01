f48e0074724c1aecaa27318f98487b6166c056f6
Sim Sun <simsun@fb.com>
2018-04-01 02:42:18 -0700

    first commit

---
+++ yasp/src/main/java/com/simsun/yasp/YASharedPreference.java
@@ -0,0 +1,66 @@
+package com.simsun.yasp;
+
+import android.content.SharedPreferences;
+import android.support.annotation.Nullable;
+
+import java.util.Map;
+import java.util.Set;
+
+public class YASharedPreference implements SharedPreferences{
+  @Override
+  public Map<String, ?> getAll() {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public String getString(String key, @Nullable String defValue) {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public Set<String> getStringSet(String key, @Nullable Set<String> defValues) {
+    return null;
+  }
+
+  @Override
+  public int getInt(String key, int defValue) {
+    return 0;
+  }
+
+  @Override
+  public long getLong(String key, long defValue) {
+    return 0;
+  }
+
+  @Override
+  public float getFloat(String key, float defValue) {
+    return 0;
+  }
+
+  @Override
+  public boolean getBoolean(String key, boolean defValue) {
+    return false;
+  }
+
+  @Override
+  public boolean contains(String key) {
+    return false;
+  }
+
+  @Override
+  public Editor edit() {
+    return null;
+  }
+
+  @Override
+  public void registerOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener) {
+
+  }
+
+  @Override
+  public void unregisterOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener) {
+
+  }
+}
